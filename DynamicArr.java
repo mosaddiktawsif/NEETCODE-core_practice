8634333340a0473427b112954dad8a6c52e30c7e
mosaddiktawsif <116061696+mosaddiktawsif@users.noreply.github.com>
2024-06-27 15:22:17 +0600

    Add files via upload

---
+++ DynamicArr.java
@@ -0,0 +1,62 @@
+class DynamicArray {
+        private int [] arr;
+        private int length ;
+        private int capacity ;
+
+    public DynamicArray(int capacity) {
+        this.capacity = capacity;
+        this.length = 0; 
+        this.arr = new int [this.capacity];
+
+    }
+
+    public int get(int i) {
+       return arr[i];  
+    }
+
+    public void set(int i, int n) {
+            arr[i]=n;  
+    }
+
+    public void pushback(int n) {
+        if ( length == capacity)
+        {
+            resize ();
+        }
+        arr[length]=n;
+        length++ ;
+
+    }
+
+    public int popback() {
+        if ( length > 0)
+        {
+            length -- ;
+        }
+        return arr[length];
+
+
+    }
+
+    private void resize() {
+        capacity *= 2;
+        int [] array = new int [capacity];
+         for (int i = 0; i<length; i++)
+         {
+            array [i]= arr[i];
+         }
+
+         arr = array;
+
+
+    }
+
+    public int getSize() {
+
+        return length;
+    }
+
+    public int getCapacity() {
+ return capacity;
+    }
+} 
